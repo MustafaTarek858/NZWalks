fe0c7f6b79e03342cd8ebcf5df332dbadf34a8dc
Moustafa.tarek <mustafaa.tarek858@gmail.com>
2025-07-08 14:31:33 +0300

    added UpdateRegionRequestDTO

--- NZWalks.API/Models/DTO/UpdateRegionRequestDTO.cs
+++ NZWalks.API/Models/DTO/UpdateRegionRequestDTO.cs
@@ -2,5 +2,8 @@
 {
     public class UpdateRegionRequestDTO
     {
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string? RegionImageUrl { get; set; }
     }
 }
